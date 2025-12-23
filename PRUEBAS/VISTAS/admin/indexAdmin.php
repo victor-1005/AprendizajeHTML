5be35afcdec0f7eeb5faab0e3fd5fa6689394f38
Victor <ml4693222@gmail.com>
2025-12-22 20:03:30 -0600

    Se creo la rama Administrador, y se creo el incio de sesión para Administradores en el indexAdmin

---
+++ PRUEBAS/VISTAS/admin/indexAdmin.php
@@ -0,0 +1,46 @@
+<!-- PHP -->
+ <?php
+    session_start();
+    if(!isset($_SESSION['usuario'])){
+        header("Location: ../.php?error=sesion");
+        exit;
+    }
+    //Evitar volver atrás después del logout
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: 0");
+    
+    //Incluimos la conexion a la BD
+    include("../../conexion.php");
+    //Preparamos una query para obtener el id Del Admin, por defecto viene como idUsuario
+    $idAdmin=$_SESSION["idUsuario"];
+    //Preparamos una Consulta para obtener los datos del Admin
+    $queryDatosAdmin=$conexion->prepare("SELECT * FROM rol WHERE idUsuario=?");
+    $queryDatosAdmin->bind_param("i",$idAdmin);
+    $queryDatosAdmin->execute();
+    $ResultadoDatosAdmin=$queryDatosAdmin->get_result();
+    $DatosAdmin=$ResultadoDatosAdmin->fetch_assoc()
+ ?>
+<!--HTML-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Administrador</title>
+    <link rel="stylesheet" href="../../css/stilos.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido <?=$DatosAdmin['usuario']?>
+            Al index de Administrador
+        </h1>
+    </header>
+    <div class="Contenedor">
+        <section>
+            <article>
+                Hola <?= $DatosAdmin['usuario']?>
+            </article>
+        </section>
+    </div>
+</body>
+</html>
