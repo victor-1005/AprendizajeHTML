e29b5fed10e9b6cfe35a1670ad8962646281d2b0
Victor <ml4693222@gmail.com>
2025-12-28 16:44:41 -0600

    Se experimento un poco mas de las funciones de js

--- JAVASCRIPT/js/eventos.js
+++ JAVASCRIPT/js/eventos.js
@@ -1,4 +1,5 @@
 //PARA SABER SI EXISTE EL BUTTON SE ACCIONA PARA QUE NO ROMPA EL JS
+console.log("eventos.js Cargado");
 const btnCambiarTexto=document.getElementById("btnCambiarTexto");
 if(btnCambiarTexto){
     function cambiar(event){
@@ -16,6 +17,34 @@ if(btnCambiarTexto){
     }
     //adicionamos la accion
     document.getElementById("btnCambiarTexto").addEventListener("click",cambiar);
+}
+//Texto directo en consola
+const textoConsola=document.getElementById("consol_log");
+if(textoConsola){
+    let txt= document.getElementById("consol_log");
+    function texto(){
+        console.group("Información a mostrar");
+        console.log("Esto sirve para mostrar información en cosola agrupada")
+        console.groupEnd();
+        return "Esto aparece en la consola\n no es necesario crear una funcion";
+    }
+    function escribir(){
+        document.getElementById("consol_log").innerText=texto();
+    }
+    document.getElementById("Accionar").addEventListener("click",escribir);
+}
+
+//Texto De asertos
+const textoAserto=document.getElementById("Asertos");
+if(textoAserto){
+    function Aserto(){
+        console.assert(5<10,"5 es menor que 10");
+        //No hace nada
+        console.assert(5<0,"5 es menor que 0");
+        // Muestra el mensaje indicado con un aviso de error
+        document.getElementById("Asertos").innerText="Esto es como una asertación de condicion solo menciona si es falso o que bajo consola";
+    }
+    document.getElementById("Accionar_Asertos").addEventListener("click",Aserto);
 }
 
 //FUNCIONES AUXILARES
